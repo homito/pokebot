4f89b8ee949102ca30221c68403081b4fbaa6798
Yoann Mahiout <mahiout@et.esiea.fr>
2025-03-26 11:09:04 +0100

    better implemented type handling using dict

--- main.py
+++ main.py
@@ -12,6 +12,27 @@ URL_POKEDEX_ICON = "https://archives.bulbagarden.net/media/upload/thumb/6/61/DP_
 URL_SPRITE = "https://play.pokemonshowdown.com/sprites"
 WEBSOCKET = "wss://sim3.psim.us/showdown/websocket"
 
+type_colors = {
+    "Normal": "\x1B[1;47;30m Normal \x1B[0m",
+    "Fighting": "\x1B[1;41m Fighting \x1B[0m",
+    "Flying": "\x1B[1;45m Flying \x1B[0m",
+    "Poison": "\x1B[1;35m Poison \x1B[0m",
+    "Ground": "\x1B[1;41m Ground \x1B[0m",
+    "Rock": "\x1B[1;41m Rock \x1B[0m",
+    "Bug": "\x1B[1;32m Bug \x1B[0m",
+    "Ghost": "\x1B[1m Ghost \x1B[0m",
+    "Steel": "\x1B[1;46m Steel \x1B[0m",
+    "Fire": "\x1B[1;41m Fire \x1B[0m",
+    "Water": "\x1B[1;45m Water \x1B[0m",
+    "Grass": "\x1B[1;32m Grass \x1B[0m",
+    "Electric": "\x1B[1;33m Electric \x1B[0m",
+    "Psychic": "\x1B[1;35m Psychic \x1B[0m",
+    "Ice": "\x1B[1;45m Ice \x1B[0m",
+    "Dragon": "\x1B[1;45;30m Dragon \x1B[0m",
+    "Dark": "\x1B[1;40m Dark \x1B[0m",
+    "Fairy": "\x1B[1;35m Fairy \x1B[0m"
+}
+
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -41,7 +62,7 @@ def pokemon_type(message: str) -> str:
     img = soup.find_all('img')
     res = "```ansi\n"
     for i in img:
-        res += f"\x1B[2;45m {i['alt']} \x1B[0m"
+        res += type_colors[i['alt']] + " "
     res += "```"
     return res
     #return "```ansi\n\x1B[2;41m Feu \x1B[0m \x1B[2;45m Vol \x1B[0m```" #TODO: not placeholder
@@ -57,13 +78,14 @@ async def foo(ctx, arg):
 
 @bot.command()
 async def dex(ctx, arg):
-    search = parse_pokemon_search(request_pokemon_search(arg)).lower()
+    message = request_pokemon_search(arg)
+    search = parse_pokemon_search(message).lower()
     response = requests.get(URL_POKEDEX, timeout=10)
     data = response.json()
     try:
         embed = discord.Embed(
             title=f"{data.get(search).get('num')}. {search.capitalize()}",
-            description= pokemon_type(request_pokemon_search(arg)),
+            description= pokemon_type(message),
             color=discord.Color.blue()
         )
         embed.set_author(name="Pokedex", icon_url=URL_POKEDEX_ICON)
