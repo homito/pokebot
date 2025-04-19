3e2a288d5b6adda816b39a562e501d448a1342d0
Yoann Mahiout <mahiout@et.esiea.fr>
2025-04-19 15:46:32 +0200

    implemented websocket class for websocket calls

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import logging
 import json
 from argparse import ArgumentParser
-from websockets.sync.client import connect
+import websockets
 import requests
 import discord
 from discord.ext import commands
@@ -11,6 +11,7 @@ URL_POKEDEX = "https://play.pokemonshowdown.com/data/pokedex.json"
 URL_POKEDEX_ICON = "https://archives.bulbagarden.net/media/upload/thumb/6/61/DP_Pok%C3%A9dex.png/726px-DP_Pok%C3%A9dex.png"
 URL_SPRITE = "https://play.pokemonshowdown.com/sprites"
 WEBSOCKET = "wss://sim3.psim.us/showdown/websocket"
+ACTION_URL = "https://play.pokemonshowdown.com/action.php"
 
 TYPE_COLORS = {
     "Normal": "\x1B[1;47;30m Normal \x1B[0m",
@@ -67,23 +68,74 @@ class DuelRequest(discord.ui.View):
         if interaction.user == self.duelee:
             await interaction.response.send_message(f"{self.duelee} declined the duel")
 
+class Websocket():
+    @classmethod
+    async def create(cls, username, password=None):
+        self = Websocket()
+        self.username = username
+        self.password = password
+        self.log_path = None # jsp a quel point c important on triera plus tard
+        self.challstr = ""
+        self.battle_id = ""
+        self.player = ""
+        self.rqid = 0
+        self.websocket = await websockets.connect(WEBSOCKET)
+        await self.connect()
+        await self.login()
+        message = await self.websocket.recv() #updateuser (jsp exactement ce que c)
+        message = await self.websocket.recv() #updatesearch
+        #await self.challenge("homiboot", "gen9randombattle")
+        return self
+    async def connect(self):
+        updateuser = await self.websocket.recv()
+        challstr = await self.websocket.recv()        
+        self.challstr = challstr.split("|challstr|")[1]
+        print("connected with challstr:", self.challstr)
+    async def login(self):
+        data = {
+            "act": "getassertion",
+            "userid": self.username,
+            "challstr": self.challstr
+        } if self.password is None else {
+            "act": "login",
+            "name": self.username,
+            "pass": self.password,
+            "challstr": self.challstr
+        }
+        res = requests.post(ACTION_URL, data=data)
+        if res.status_code != 200:
+            print("Login failed")
+            return
+        print("Login success")
+        if self.password is None:
+            assertion = res.text
+        else:
+            assertion = json.loads(res.text[1:])["assertion"]
+        change_name = f"|/trn {self.username},0,{assertion}"
+        await self.websocket.send(change_name)
+        response = await self.websocket.recv()
+        print(response)
+    async def challenge(self, opponent, battleformat):
+        await self.websocket.send(f"|/challenge {opponent}, {battleformat}")
+        response = await self.websocket.recv()
+        print(response)
+    async def request_pokemon_search(self, searched: str):
+        """
+        This function will send a message to the websocket
+        and return the response
+        """
+        await self.websocket.send(f"|/dt {searched}")
+        message = await self.websocket.recv()
+        print(message)
+        return message
+
+ws = None
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
-def request_pokemon_search(searched: str):
-    """
-    This function will send a message to the websocket
-    and return the response while ignoring the first two messages
-    """
-    with connect(WEBSOCKET) as websocket:
-        websocket.recv() # ignore |updateuser| message
-        websocket.recv() # ignore |challstr| message (i should probably check what they are so its not hacked like that)
-        websocket.send(f"|/dt {searched}")
-        message = websocket.recv()
-        return message
 def parse_pokemon(message:str):
     m = [w for w in message.split("|pm|") if "pokemonnamecol" in w]
     html = m[0].split("/raw ")[1] # if websocket sends error, this will beak as there is nothing to split
@@ -112,32 +164,22 @@ async def on_ready():
     print("Pokedex data loaded")
     logging.info("Pokedex data loaded")
 
+    #create the websocket connection
+    global ws
+    ws = await Websocket.create("homiboot")
+    print("Websocket connection created")
+    logging.info("Websocket connection created")
+
 @bot.command()
 async def duel(ctx, arg):
     dueler = ctx.message.author
     duelee = ctx.message.mentions[0]
     await ctx.send(f"{dueler.mention} wants to duel {duelee.mention}", view=DuelRequest(dueler=dueler, duelee=duelee, timeout=60))
 
-class DuelRequest(discord.ui.View):
-    dueler = None
-    duelee = None
-    def __init__(self, dueler, duelee, timeout=60):
-        super().__init__(timeout=timeout)
-        self.dueler = dueler
-        self.duelee = duelee
-    @discord.ui.button(label="Accept", style=discord.ButtonStyle.primary)
-    async def accept(self, interaction, button):
-        if interaction.user == self.duelee:
-            await interaction.response.send_message("You pressed me!")
-    @discord.ui.button(label="Decline", style=discord.ButtonStyle.danger)
-    async def decline(self, interaction, button):
-        if interaction.user == self.duelee:
-            await interaction.response.send_message(f"{self.duelee} declined the duel")
-
 @bot.command()
 async def dex(ctx, arg):
     #get the message from the websocket
-    message = request_pokemon_search(arg)
+    message = await ws.request_pokemon_search(arg)
     #parse the message
     soup = parse_pokemon(message)
     #get the pokemon name
